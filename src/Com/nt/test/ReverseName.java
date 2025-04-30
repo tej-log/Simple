883ea5fe3dc10ebabd57d13a4fc9d707a620ce68
tejas <tejpatil354@gmail.com>
2025-04-30 16:23:20 +0530

    developera story compleate

--- src/Com/nt/test/ReverseName.java
+++ src/Com/nt/test/ReverseName.java
@@ -1,7 +1,13 @@
 package Com.nt.test;
 
 public class ReverseName {
-public static void main(String[] args) {
-	String str ="tejas";
-}
+	public static void main(String[] args) {
+		String str = "tejas";
+		int leng = str.length();
+		for(int i=leng-1;i>=0;i--) {
+			System.out.println();
+			System.out.println();
+			
+		}
+	}
 }
