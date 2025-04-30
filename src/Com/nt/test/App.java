877fbec53cfc1c05a2a58ea8362fc16c4db1d85f
tejas <tejpatil354@gmail.com>
2025-04-30 15:59:08 +0530

    Third commit

---
+++ src/Com/nt/test/App.java
@@ -0,0 +1,9 @@
+package Com.nt.test;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("main");
+		System.out.println("next main");
+	}
+
+}
---
+++ src/Com/nt/test/ReverseName.java
@@ -0,0 +1,7 @@
+package Com.nt.test;
+
+public class ReverseName {
+public static void main(String[] args) {
+	String str ="tejas";
+}
+}
