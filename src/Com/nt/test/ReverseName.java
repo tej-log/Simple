f6752521244c3e3de25d0e2402449c1285adce11
tejas <tejpatil354@gmail.com>
2025-04-30 17:02:17 +0530

    developer A story compilted

--- src/Com/nt/test/ReverseName.java
+++ src/Com/nt/test/ReverseName.java
@@ -3,11 +3,17 @@ package Com.nt.test;
 public class ReverseName {
 	public static void main(String[] args) {
 		String str = "tejas";
+		String rev = " ";
 		int leng = str.length();
-		for(int i=leng-1;i>=0;i--) {
-			System.out.println();
-			System.out.println();
-			
+		for (int i = leng - 1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
+			System.out.println(rev);
+			StringBuffer sb = new StringBuffer(str);
+			System.out.println(sb.reverse());
+			System.out.println(sb);
+			StringBuilder sb1 = new StringBuilder(str);
+			System.out.println(sb1.reverse());
 		}
+
 	}
 }
